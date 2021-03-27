9e25d8faab935d1c294e0a28f87aa047675317ef
Jorge <jepers2021@gmail.com>
2021-03-26 22:26:09 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace A133590.Ejercicio12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 12");
+            Console.WriteLine("Dadas dos frases, determina si son iguales. (Es sensible a mayúsculas y minúsculas)");
+            Console.Write("Ingrese una frase: ");
+            string unaFrase = Console.ReadLine();
+            Console.Write("Ingrese otra frase: ");
+            string otraFrase = Console.ReadLine();
+
+            if (unaFrase.Equals(otraFrase))
+            {
+                Console.WriteLine("Las frases son iguales.");
+            }
+            else
+            {
+                Console.WriteLine("Las frases no son iguales.");
+            }
+
+
+            Console.WriteLine("Presione cualquier tecla para salir..");
+            Console.ReadKey();
+        }
+    }
+}
